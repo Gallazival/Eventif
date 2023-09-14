28e40f14da041ff410fb847d42c62bbbd38f4104
Enzo Hsu <61277330+Gallazival@users.noreply.github.com>
2023-09-14 02:08:58 -0300

    O COMEÇO DO FINAL

--- contact/tests/test_views_contact.py
+++ contact/tests/test_views_contact.py
@@ -1,25 +1,20 @@
 from django.core import mail
 from django.test import TestCase
+from contact.forms import contact_form
 
-from subscriptions.forms import SubscriptionForm
-
-
-class SubscribeGet(TestCase):
+class teste_get(TestCase):
     def setUp(self):
-        self.response = self.client.get('/inscricao/')
+        self.response = self.client.get('/contato/')
 
     def test_get(self):
-        """GET /inscricao/ must return status_code 200"""
-        response = self.client.get('/inscricao/')
+        response = self.client.get('/contato/')
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        """Must use subscriptions/subscription_fotm.html"""
         self.assertTemplateUsed(
             self.response, 'subscriptions/subscription_form.html')
 
     def test_html(self):
-        """HTML must contain 5 input tags"""
         tags = (('<form', 1),
                 ('<input', 6),
                 ('type="text"', 3),
@@ -30,52 +25,38 @@ class SubscribeGet(TestCase):
                 self.assertContains(self.response, text, count)
 
     def test_csrf(self):
-        """HTML must contain CSRF"""
         self.assertContains(self.response, "csrfmiddlewaretoken")
 
     def test_has_form(self):
-        """Context must have subscription form"""
         form = self.response.context["form"]
-        self.assertIsInstance(form, SubscriptionForm)
+        self.assertIsInstance(form, contact_form)
 
-class SubscribePostValid(TestCase):
+class teste_post_e_valido(TestCase):
     def setUp(self):
-        data = dict(name="Enzo Hsu",
-                    cpf="12345678901",
-                    email="enzo.hsu@aluno.riogrande.ifrs.edu.br",
-                    phone="53-99973-1504")
-        self.response = self.client.post('/inscricao/', data)
+        data = {'nome' : "Enzo Hsu",
+                'email' : "enzo.hsu@aluno.riogrande.ifrs.edu.br",
+                'numero' : "53-99973-1504",
+                'mensagem' : "Funcionou!"}
+        self.response = self.client.post('/contato/', data)
 
     def test_post(self):
         self.assertEqual(302, self.response.status_code)
 
-    def test_send_subscribe_email(self):
-        self.assertEqual(1, len(mail.outbox))
+    def test_enviou_email(self):
+        self.assertTrue(mail.outbox)
 
 
-class SubscribePostInvalid(TestCase):
+class teste_post_e_invalido(TestCase):
     def setUp(self):
-        self.response = self.client.post('/inscricao/', {})
+        self.response = self.client.post('/contato/', {})
 
     def test_post(self):
         self.assertEqual(200, self.response.status_code)
 
-    def test_template(self):
-        self.assertTemplateUsed(
-            self.response, 'subscriptions/subscription_form.html')
-
     def test_has_form(self):
         form = self.response.context['form']
-        self.assertIsInstance(form, SubscriptionForm)
+        self.assertIsInstance(form, contact_form)
 
     def test_form_has_error(self):
         form = self.response.context['form']
         self.assertTrue(form.errors)
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name="Enzo Hsu", cpf="12345678901",
-                    email="enzo.hsu@aluno.riogrande.ifrs.edu.br", phone="53-99973-1504")
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
